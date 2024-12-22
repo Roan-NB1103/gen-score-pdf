63e6822380003a85664e5f044bd16687bd880ac7
Roan-NB1103 <86891543+Roan-NB1103@users.noreply.github.com>
2024-12-23 07:11:15 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-playwright install
-playwright install-deps
+
+# システム更新とPythonのセットアップ
+apt-get update
+apt-get install -y python3-pip
+
+# Playwrightのインストール
+pip install playwright
+
+# Playwrightブラウザのインストール
+playwright install chromium
+playwright install-deps chromium
+
+# 権限の設定
+chmod -R 777 /home/appuser/.cache/ms-playwright
